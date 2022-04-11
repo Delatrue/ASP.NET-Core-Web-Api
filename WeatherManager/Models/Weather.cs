6d5c11a001697bcd5619bc072b42ef89927c3d92
Eugene <gitdelatrue@gmail.com>
2022-04-12 01:21:10 +0500

    CRUD Controller FirstMicroservice

---
+++ WeatherManager/Models/Weather.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WeatherManager.Models
+{
+    public class Weather
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public float Temperature { get; set; }
+    }
+}
