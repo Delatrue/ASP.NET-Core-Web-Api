4ca31e3054f12d99c91707e5376359487471405c
Eugene <gitdelatrue@gmail.com>
2022-04-17 01:32:19 +0500

    Rest API and Test

---
+++ MetricsManager 1.0/MetricsAgent/Controllers/HddMetricsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace MetricsAgent.Controllers
+{
+    [Route("api/metrics/hdd")]
+    [ApiController]
+    public class HddMetricsController : ControllerBase
+    {
+        [HttpGet("api/metrics/hdd/left/from/{fromTime}/to/{toTime}")]
+        public IActionResult GetMetricsFromAgent(
+            [FromRoute] int agentId,
+            [FromRoute] TimeSpan fromTime,
+            [FromRoute] TimeSpan toTime)
+        {
+            return Ok();
+        }
+
+
+        [HttpGet("cluster/from/{fromTime}/to/{toTime}")]
+        public IActionResult GetMetricsFromAllCluster(
+            [FromRoute] TimeSpan fromTime,
+            [FromRoute] TimeSpan toTime)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ MetricsManager 1.0/MetricsManagerTests/RamMetricsControllerUnitTests.cs
@@ -0,0 +1,31 @@
+﻿using MetricsManager_1._0.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Xunit;
+namespace MetricsManagerTests
+{
+    public class RamMetricsControllerUnitTests
+    {
+        private RamMetricsController controller;
+        public RamMetricsControllerUnitTests()
+        {
+            controller = new RamMetricsController();
+        }
+        [Fact]
+        public void GetMetricsFromAgent_ReturnsOk()
+        {
+            //Arrange
+            var agentId = 1;
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+
+            //Act
+            var result = controller.GetMetricsFromAgent(
+                agentId,
+                fromTime,
+                toTime);
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+    }
+}
